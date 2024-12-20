dbd0f50aaa022ce64fc57c4e76db48928b16cb8f
duanzw <duanzhongwei@jcble.com>
2024-12-20 15:50:46 +0800

    1.打开设备

--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,17 @@
 #include <fcntl.h>
 #include <unistd.h>
 
+using namespace std;
 int main(int argc, char* argv[])
 {
     string device = "/dev/video11";
     /* 打开设备 */
     int fd = open("/dev/video11",O_RDWR);
     if(fd < 0){
-        std::cout << "打开设备失败" << std::endl;
+        cout << "打开设备失败" << endl;
         return -1;
     }
-    std::cout << "打开设备成功" << std::endl;
+    cout << "打开设备成功" << endl;
 
     /* 最后一步:关闭设备 */
     close(fd);
