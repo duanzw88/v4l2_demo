534e1ffe1f75dd68f108367cfa0da4ab902fe97b
duanzw <duanzhongwei@jcble.com>
2024-12-20 15:49:04 +0800

    1.打开设备

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 int main(int argc, char* argv[])
 {
-    std::cout << "Hello world" << std::endl;
+    string device = "/dev/video11";
+    /* 打开设备 */
+    int fd = open("/dev/video11",O_RDWR);
+    if(fd < 0){
+        std::cout << "打开设备失败" << std::endl;
+        return -1;
+    }
+    std::cout << "打开设备成功" << std::endl;
+
+    /* 最后一步:关闭设备 */
+    close(fd);
     return 0;
 }
