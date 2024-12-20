72f9b28d05b8b2d4bbce7ef4bf1b2ee2f8b1bce5
duanzw <duanzhongwei@jcble.com>
2024-12-20 15:56:01 +0800

    2.获取摄像头支持的数据格式

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <linux/videodev2.h>
 
 using namespace std;
 int main(int argc, char* argv[])
@@ -16,6 +17,24 @@ int main(int argc, char* argv[])
     }
     cout << "打开设备成功" << endl;
 
+    /* 2. 获取摄像头支持的格式ioctl(文件描述符，命令，结构体指针)*/
+    struct v4l2_fmtdesc v4fmt;
+    v4fmt.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    int i = 0;
+    while(true){
+        v4fmt.index = i++;
+        int ret = ioctl(fd,VIDIOC_ENUM_FMT,&v4fmt);
+        if(ret < 0){
+            break;
+        }
+
+        cout << "index = " << v4fmt.index;
+        cout << "flags = " << v4fmt.flags;
+        cout << "desc = " << v4fmt.description;
+        unsigned char *p = (unsigned char *)&v4fmt.pixelformat;
+        cout << "pixelformat = " << p[0] << p[1] << p[2] << p[3];
+        cout << "reserved = " << v4fmt.reserved[0];
+    }
     /* 最后一步:关闭设备 */
     close(fd);
     return 0;
