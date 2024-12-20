05c024f1d4e8ac456bb58b3a7ab488c33b48c856
duanzw <duanzhongwei@jcble.com>
2024-12-20 16:30:18 +0800

    3.设置采集格式

--- src/main.cpp
+++ src/main.cpp
@@ -44,7 +44,7 @@ int main(int argc, char* argv[])
     vfmt.type = V4L2_BUF_TYPE_VIDEO_CAPTURE; //摄像头采集
     vfmt.fmt.pix.width = 3840; //设置采集宽度
     vfmt.fmt.pix.height = 2160; //设置采集高度
-    vfmt.fmt.pix.pixelformat = V4L2_PIX_FMT_NV12M; //设置采集格式
+    vfmt.fmt.pix.pixelformat = V4L2_PIX_FMT_UYUV; //设置采集格式
     // vfmt.fmt.pix.field = V4L2_FIELD_ANY; //设置采集场
     ret = ioctl(fd,VIDIOC_S_FMT,&vfmt);
     if(ret < 0){
